460faf901ce4df9743e1ef8370072a751aade4d5
vikas-saini-7 <vikas289888@gmail.com>
2024-09-12 19:44:54 +0530

    small change

--- app/[id]/people/page.tsx
+++ app/[id]/people/page.tsx
@@ -34,19 +34,22 @@ const people: IPerson[] = [
 
 const page: React.FC = () => {
   return (
-    <div className="p-4 grid grid-cols-3 gap-2">
-      {people.map((item) => (
-        <div
-          className="relative w-full py-2 min-h-14 bg-gray-500/10 rounded flex gap-1 flex-col items-center justify-center"
-          key={item.id}
-        >
-          <span className="absolute top-1 left-1 w-2 h-2 rounded-full bg-green-500"></span>
-          <span className="w-6 h-6 rounded-full bg-gray-500 flex items-center justify-center">
-            {item.name.charAt(0).toUpperCase()}
-          </span>
-          <p className="text-xs">{item.name}</p>
-        </div>
-      ))}
+    <div className="p-4">
+      <h1 className="mb-2">Active Users</h1>
+      <div className="grid grid-cols-3 gap-2">
+        {people.map((item) => (
+          <div
+            className="relative w-full py-2 min-h-14 bg-gray-500/10 rounded flex gap-1 flex-col items-center justify-center"
+            key={item.id}
+          >
+            <span className="absolute top-1 left-1 w-2 h-2 rounded-full bg-green-500"></span>
+            <span className="w-6 h-6 rounded-full bg-gray-500 flex items-center justify-center">
+              {item.name.charAt(0).toUpperCase()}
+            </span>
+            <p className="text-xs">{item.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
