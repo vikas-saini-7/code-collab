0b29b8e01507012b3cd486c9dcaa80eca0da360d
vikas-saini-7 <vikas289888@gmail.com>
2025-06-19 12:17:22 +0530

    cors

--- socket/server.js
+++ socket/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 // Set up Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     methods: ["GET", "POST"],
   },
 });
