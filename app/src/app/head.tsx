f4d955fa234373af3c168f0bc8516de58903b999
vikas-saini-7 <vikas289888@gmail.com>
2025-06-20 23:56:28 +0530

    favicon

---
+++ app/src/app/head.tsx
@@ -0,0 +1,31 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Code Collab</title>
+      <meta
+        name="description"
+        content="Real-time code collaboration platform"
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+    </>
+  );
+}
